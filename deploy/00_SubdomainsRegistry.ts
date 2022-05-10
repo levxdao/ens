e301dc4bff9ddc6f1ace0e8eb21bf0450563ec0e
LevX <team@levx.io>
2022-05-11 07:08:18 +0900

    Update

--- deploy/00_SubdomainsRegistry.ts
+++ deploy/00_SubdomainsRegistry.ts
@@ -1,6 +1,6 @@
 import { ethers } from "hardhat";
 
-const TOKEN = "0xf474E526ADe9aD2CC2B66ffCE528B1A51B91FCdC";
+const TOKEN = "0xdE0C1dC7f2b67705Cca50039418715F9C7F8D53B";
 const ENS = "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e";
 const NODE = "0xf88903d82aebfe9a5fa03a1a6eb4475330ed9991c9b6ffea0f6d0154a210efbe";
 const RESOLVER = "0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41";
@@ -11,7 +11,7 @@ export default async ({ getNamedAccounts, deployments }) => {
 
     const { address } = await deploy("SubdomainsRegistry", {
         from: deployer,
-        args: [TOKEN, ENS, NODE, RESOLVER, 1654041600],
+        args: [TOKEN, ENS, NODE, RESOLVER],
         log: true,
     });
 
